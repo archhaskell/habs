e7473a34dacf1a7e36a0f4b0a480693b04dbb0d0
Magnus Therning <magnus@therning.org>
2013-02-01 21:46:03 +0100

    Update the adjust script for the new GHC.
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- ghc/helpers/adjust-ghc.sh
+++ ghc/helpers/adjust-ghc.sh
@@ -3,5 +3,5 @@
 # modify the relver of ghc, this saves on recompilations of cblrepo
 
 for f in $*; do
-  sed -e 's/\(.*\)ghc=7\.4\.2-1\(.*\)/\1ghc=7.6.1-1\2/' -i $f
+  sed -e 's/\(.*\)ghc=7\.6\.1-1\(.*\)/\1ghc=7.6.2-1\2/' -i $f
 done
