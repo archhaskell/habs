51e9f4e071976ed8e7545c9d0929685b156d4af1
Magnus Therning <magnus@therning.org>
2014-02-09 17:18:52 +0100

    Fix the script for adjusting ghc dependency.
    Useful until cblrepo has caught up.
    
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- ghc/helpers/adjust-ghc.sh
+++ ghc/helpers/adjust-ghc.sh
@@ -3,5 +3,6 @@
 # modify the relver of ghc, this saves on recompilations of cblrepo
 
 for f in $*; do
-  sed -e 's/\(.*\)ghc=7\.6\.1-1\(.*\)/\1ghc=7.6.2-1\2/' -i $f
+  sed -e 's/ghc=7\.6\.3-1/ghc=7.8.0-0.1/' -i $f
+  sed -e 's/--enable-split-objs//' -i $f
 done
