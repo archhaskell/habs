b8b67c170de3f3db199a48cfca3e07b583ea5329
Magnus Therning <magnus@therning.org>
2014-03-01 10:16:27 +0100

    Re-build of GHC 7.8.
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- ghc/helpers/adjust-ghc.sh
+++ ghc/helpers/adjust-ghc.sh
@@ -3,6 +3,6 @@
 # modify the relver of ghc, this saves on recompilations of cblrepo
 
 for f in $*; do
-  sed -e 's/ghc=7\.6\.3-1/ghc=7.8.0-0.2/' -i $f
+  sed -e 's/ghc=7\.6\.3-1/ghc=7.8.0-0.3/' -i $f
   sed -e 's/--enable-split-objs//' -i $f
 done
