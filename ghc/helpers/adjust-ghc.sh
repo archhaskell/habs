ef5724e7017e778323e4e28fdc1192f8b0edd2ad
Magnus Therning <magnus@therning.org>
2014-04-04 21:23:45 +0200

    Update ghc-mod and hlint.
    Adjust the helper script to the dev version of clbrepo.
    
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- ghc/helpers/adjust-ghc.sh
+++ ghc/helpers/adjust-ghc.sh
@@ -3,6 +3,6 @@
 # modify the relver of ghc, this saves on recompilations of cblrepo
 
 for f in $*; do
-  sed -e 's/ghc=7\.6\.3-1/ghc=7.8.0.20140228-1/' -i $f
+  sed -e 's/ghc=7\.8\.0-1/ghc=7.8.0.20140228-1/' -i $f
   sed -e 's/--enable-split-objs//' -i $f
 done
