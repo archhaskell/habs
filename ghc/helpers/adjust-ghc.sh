45deb8017bf39e13d0ba891a683ac281234b6112
Magnus Therning <magnus@therning.org>
2014-07-13 13:28:34 +0200

    Adjusting the patches to the bump.
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- ghc/helpers/adjust-ghc.sh
+++ ghc/helpers/adjust-ghc.sh
@@ -3,6 +3,6 @@
 # modify the relver of ghc, this saves on recompilations of cblrepo
 
 for f in $*; do
-  sed -e 's/ghc=7\.8\.0-1/ghc=7.8.2-1/' -i $f
+  sed -e 's/ghc=7\.8\.2-1/ghc=7.8.3-1/' -i $f
   sed -e 's/--enable-split-objs//' -i $f
 done
